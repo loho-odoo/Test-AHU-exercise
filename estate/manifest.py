8d694e8dafd8bb26dc333c9d82fa81708d7abdf2
loho-odoo <86238557+loho-odoo@users.noreply.github.com>
2023-08-10 16:51:17 +0200

    Update manifest.py

--- estate/manifest.py
+++ estate/manifest.py
@@ -1,1 +1,12 @@
-
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'summary': 'Manage our real estate',
+    'description': "",
+    'depends': [
+        'base_setup',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
